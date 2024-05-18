b9ccfd3a60e20ffe69304b76aad1d345f641a685
Franziska Kunsmann <hi@kunsmann.eu>
2024-05-18 13:13:12 +0200

    add schedule.json view

--- schedule/urls.py
+++ schedule/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("schedule/<slug:slug>.json", views.schedule_json, name="schedule_json"),
     path("schedule/<slug:slug>.xml", views.schedule_xml, name="schedule_xml"),
     path("schedule/<slug:slug>.html", views.schedule_html, name="schedule_html"),
     path("", views.index, name="index"),
--- schedule/views.py
+++ schedule/views.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timedelta
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 
 from .models import Event, Room, ScheduleEntry
+
 
 
 def index(request):
@@ -75,3 +76,71 @@ def schedule_xml(request, slug):
         },
         content_type="application/xml",
     )
+
+
+def schedule_json(request, slug):
+    event = get_object_or_404(Event, acronym=slug)
+
+    schedule = {
+        "version": None,
+        "conference": {
+            "acronym": event.acronym,
+            "title": event.name,
+            "start": event.start.strftime("%Y-%m-%d"),
+            "end": (event.start + timedelta(days=event.duration_days+1)).strftime("%Y-%m-%d"),
+            "daysCount": event.duration_days,
+            "timeslot_duration": "00:05",
+            "rooms": [
+                {
+                    "name": room.name,
+                    "guid": room.uuid,
+                }
+                for room in Room.objects.filter(event=event)
+            ],
+            "tracks": [],
+            "days": [],
+        },
+    }
+
+    for day in range(0, event.duration_days):
+        day_schedule = {
+            "index": day,
+            "date": (event.start + timedelta(days=day)).strftime("%Y-%m-%d"),
+            "day_start": (event.start + timedelta(days=day)).isoformat(),
+            "day_end": (event.start + timedelta(days=day+1)).isoformat(),
+            "rooms": {},
+        }
+
+        for room in Room.objects.filter(event=event):
+            day_schedule["rooms"][room.name] = []
+            for entry in  ScheduleEntry.objects.filter(
+                room=room, start__date=event.start + timedelta(days=day)
+            ).order_by("start"):
+                day_schedule["rooms"][room.name].append({
+                    "id": entry.pk,
+                    "guid": entry.uuid,
+                    "date": entry.start.isoformat(),
+                    "start": entry.start.strftime("%H:%M"),
+                    "logo": None,
+                    "duration": entry.duration_hhmm,
+                    "room": room.name,
+                    "slug": entry.full_slug,
+                    "title": entry.title,
+                    "subtitle": "",
+                    "track": None,
+                    "type": "unknown",
+                    "abstract": entry.abstract,
+                    "description": None,
+                    "recording_license": "",
+                    "do_not_record": entry.recording_optout,
+                    "persons": [
+                        {
+                            "public_name": person.name,
+                        } for person in entry.persons.all()
+                    ],
+                    "links": [],
+                    "attachments": [],
+                })
+        schedule["conference"]["days"].append(day_schedule)
+
+    return JsonResponse(schedule)
